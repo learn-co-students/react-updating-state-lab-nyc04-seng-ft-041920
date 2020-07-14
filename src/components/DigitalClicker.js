8bfaee1086c30ac8f4f48ac6434ed794eaa33994
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-13 21:08:24 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({timesClicked: prevState.timesClicked + 1}))
+  }
+
+  // https://stackoverflow.com/questions/42364838/incrementing-state-value-by-one-using-react
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
+
+// The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,61 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+// Create a button with the class 'bitrate'. Clicking this button changes the settings.bitrate state property to 12.
+
+  // this.setState({
+  //   addressInfo: {
+  //     ...this.state.addressInfo,
+  //     city: 'New York City'
+  //   }
+  // });
+
+  handleBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+
+// Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleBitrate}>Update Bitrate</button>
+        <button className="resolution" onClick={this.handleResolution}>Update Resolution</button>
+      </div>
+    )
+  }
+
+}
